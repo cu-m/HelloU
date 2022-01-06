f7c905e8fd8a97d2d05bfcc5731ecbc06798ae93
Aesth <59161307+aesth1@users.noreply.github.com>
2022-01-06 07:56:19 +0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,50 @@
+include <stdint.h>
+include <stdbool.h>
+include <malloc.h>
+include <coreinit/screen.h>
+include <coreinit/cache.h>
+include <whb/log_cafe.h>
+include <whb/log_udp.h>
+include <whb/log.h>
+include <whb/proc.h>
+int main(int argc, char** argv) {
+    WHBLogCafeInit();
+WHBLogUdpInit();
+WHBLogPrint("Hello World! Logging initialised.");
+WHBProcInit();
+OSScreenInit();
+size_t tvBufferSize = OSScreenGetBufferSizeEx(SCREEN_TV);
+size_t drcBufferSize = OSScreenGetBufferSizeEx(SCREEN_DRC);
+WHBLogPrintf("Will allocate 0x%X bytes for the TV, " \
+             "and 0x%X bytes for the DRC.",
+             tvBufferSize, drcBufferSize);
+             void* tvBuffer = memalign(0x100, tvBufferSize);
+void* drcBuffer = memalign(0x100, drcBufferSize);
+OSScreenSetBufferEx(SCREEN_TV, tvBuffer);
+OSScreenSetBufferEx(SCREEN_DRC, drcBuffer);
+OSScreenEnableEx(SCREEN_TV, true);
+OSScreenEnableEx(SCREEN_DRC, true);
+while(WHBProcIsRunning()) {
+    OSScreenClearBufferEx(SCREEN_TV, 0x00000000);
+OSScreenClearBufferEx(SCREEN_DRC, 0x00000000);
+OSScreenPutFontEx(SCREEN_TV, 0, 0, "Rashed is the best");
+OSScreenPutFontEx(SCREEN_TV, 0, 1, "Hello!");
+
+OSScreenPutFontEx(SCREEN_DRC, 0, 0, "Rashed is the best");
+OSScreenPutFontEx(SCREEN_DRC, 0, 1, "Hello!");
+DCFlushRange(tvBuffer, tvBufferSize);
+DCFlushRange(drcBuffer, drcBufferSize);
+OSScreenFlipBuffersEx(SCREEN_TV);
+OSScreenFlipBuffersEx(SCREEN_DRC);
+}
+WHBLogPrint("Got shutdown request!");
+if (tvBuffer) free(tvBuffer);
+if (drcBuffer) free(drcBuffer);
+OSScreenShutdown();
+WHBProcShutdown();
+
+WHBLogPrint("Quitting.");
+WHBLogCafeDeinit();
+WHBLogUdpDeinit();
+    return 1;
+}
